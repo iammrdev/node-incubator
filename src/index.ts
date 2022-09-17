76af4a775b4ae08cba74eb83f2c3ba93e9d204f6
Vitalii Iashchuk <iashchuk@hotmail.com>
2022-09-17 19:13:37 +0300

    S01: add get videos endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,25 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+const videos = [{
+  "id": 0,
+  "title": "string",
+  "author": "string",
+  "canBeDownloaded": true,
+  "minAgeRestriction": null,
+  "createdAt": "2022-09-17T16:09:46.178Z",
+  "publicationDate": "2022-09-17T16:09:46.178Z",
+  "availableResolutions": [
+    "P144"
+  ]
+}]
+
 app.get("/", (_req: Request, res: Response) => {
   res.send("Hello World!!!");
+});
+
+app.get("/videos", (_req: Request, res: Response) => {
+  res.send(videos);
 });
 
 app.listen(port, () => {
