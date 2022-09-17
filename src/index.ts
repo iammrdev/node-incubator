ce681e6f6809b05b136ab6fd18bbd50a5efad771
Vitalii Iashchuk <iashchuk@hotmail.com>
2022-09-17 20:08:19 +0300

    S01: add new app endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,91 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+
+let id = 2;
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const videos = [{
-  "id": 0,
-  "title": "string",
-  "author": "string",
-  "canBeDownloaded": true,
-  "minAgeRestriction": null,
-  "createdAt": "2022-09-17T16:09:46.178Z",
-  "publicationDate": "2022-09-17T16:09:46.178Z",
-  "availableResolutions": [
-    "P144"
-  ]
-}]
+app.use(express.json());
 
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello World!!!");
+interface Video {
+    id: number;
+    title: string;
+    author: string;
+    canBeDownloaded: boolean;
+    minAgeRestriction: number | null;
+    createdAt: Date;
+    publicationDate: Date;
+    availableResolutions: string[];
+}
+
+const videos: Video[] = [
+    {
+        id: 1,
+        title: 'string',
+        author: 'string',
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        createdAt: new Date(),
+        publicationDate: new Date(),
+        availableResolutions: ['P144'],
+    },
+];
+
+app.get('/', (_req: Request, res: Response) => {
+    res.send('Hello World!!!');
 });
 
-app.get("/videos", (_req: Request, res: Response) => {
-  res.send(videos);
+app.get('/videos', (_req: Request, res: Response) => {
+    res.send(videos);
+});
+
+app.post('/videos', (req: Request, res: Response) => {
+    const data = req.body;
+
+    const video: Video = {
+        id: id++,
+        title: data.title,
+        author: data.author,
+        canBeDownloaded: data.canBeDownloaded || true,
+        minAgeRestriction: data.minAgeRestriction || null,
+        createdAt: new Date(),
+        publicationDate: new Date(),
+        availableResolutions: data.availableResolutions || [],
+    };
+
+    videos.push(video);
+
+    res.status(201).send(video);
+});
+
+app.get('/videos/:id', (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    const video = videos.find((item) => item.id === id);
+
+    if (!video) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.send(video);
+});
+
+app.delete('/videos/:id', (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    const index = videos.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+        res.sendStatus(404);
+        return;
+    }
+
+    videos.splice(index, 1);
+
+    res.sendStatus(204);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
-
